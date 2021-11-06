586f200df5d0eff1ecf380af0018b0353f055c8f
Richard Lam <54393902+richlamdev@users.noreply.github.com>
2021-11-05 17:44:22 -0700

    added get_integration_keys function

--- pd_client.py
+++ pd_client.py
@@ -7,36 +7,49 @@ from pathlib import Path
 import re
 
 
-def get_services(api_token, query):
+def get_services (session, query):
 
-    session = APISession (api_token)
+    #session = APISession (api_token)
     services = list(session.iter_all('services', params={'query': query} ))
     #print (services)
-    services_dict = dict (services[0])
-    print (json.dumps(services_dict, indent=4, sort_keys=False))
-    print ()
-    print ()
-    print ()
-
-    print (json.dumps(services_dict["integrations"][0]["self"]))
-
-    service_url = json.dumps(services_dict["integrations"][0]["self"])
-
-    base_url = "https://api.pagerduty.com/"
-
-    endpoint_url_only = re.sub (base_url,"",service_url)
-
-    print (endpoint_url_only)
-
-    print ()
-
-    print ()
-    print ()
 
     return services
 
-    #with open ("temp.json", "w") as tempfile:
-        #json.dump (services, tempfile, ensure_ascii=False,sort_keys=True, indent=4)
+def get_service_urls (all_services):
+
+    integration_urls = []
+    service_urls = []
+    endpoint_url_only = []
+
+    for service in range (len (all_services)):
+        integration_urls.append ( dict (all_services[service]) )
+        #print (json.dumps (integration_urls[service], indent=4, sort_keys=False))
+        #print (json.dumps (integration_urls[service]["integrations"][0]["self"]))
+        service_urls.append (json.dumps(integration_urls[service]["integrations"][0]["self"]))
+        base_url = "https://api.pagerduty.com/"
+        endpoint_url_only.append (re.sub (base_url,"",service_urls[service]))
+        #print (endpoint_url_only[service])
+
+    return endpoint_url_only
+
+def get_integration_keys (session, all_integrations):
+
+    #print (json.loads(integration_urls[0]))
+
+    integration_info = []
+    integration_keys = []
+    integration_name = []
+
+    for service in range (len (all_integrations)):
+        integration_info.append (session.rget (json.loads(all_integrations[service])))
+        #print (json.dumps(integration_info[service],indent=4))
+        integration_name.append (integration_info[service]["service"]["summary"])
+        print (integration_name[service])
+
+        #integration_keys.append (integration_info[service]["integration_key"])
+        #print (integration_keys[service])
+
+
 
 def check_api_key():
 
@@ -77,16 +90,28 @@ def check_api_key():
 def main():
 
     all_services = []
+    integration_urls = []
 
     api_token = check_api_key()
     #print (api_token)
 
-    all_services = get_services(api_token, "*ETS")
+    session = APISession (api_token)
 
-    for service in all_services:
-        print (service)
+    all_services = get_services(session, "*Network")
 
-    print ("number of services: " + str(len(all_services)) )
+    integration_urls = get_service_urls (all_services)
+    #print (integration_urls)
+
+    get_integration_keys (session, integration_urls)
+
+
+    #for service in all_services:
+        #integration_urls[service] = 
+
+    #for service in all_services:
+        #print (service)
+
+    #print ("number of services: " + str(len(all_services)) )
 
 
 if __name__ == "__main__":
